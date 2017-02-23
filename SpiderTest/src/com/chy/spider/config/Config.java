3456a79be3ebef24943d9592442f60e3cd0d3c76
chy <cccyyy2000@163.com>
2017-02-23 14:31:23 +0800

    add config classes

---
+++ SpiderTest/src/com/chy/spider/config/Config.java
@@ -0,0 +1,10 @@
+package com.chy.spider.config;
+
+import com.chy.spider.utils.ConfigHelper;
+
+public class Config {
+
+	public static final int DefaultMaxDepth=Integer.parseInt(ConfigHelper.getParameter("defaultMaxDepth"));
+	public static final int DefaultMaxVisitNum=Integer.parseInt(ConfigHelper.getParameter("defaultMaxVisitNum"));
+
+}
---
+++ SpiderTest/src/com/chy/spider/utils/ConfigHelper.java
@@ -0,0 +1,62 @@
+package com.chy.spider.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class ConfigHelper {
+
+	private static Log log = LogFactory.getLog(ConfigHelper.class);
+	private static Properties properties = new Properties();
+	private static String filePath;
+	
+	static{
+		properties=getProperties();
+	}
+	
+	private static Properties getProperties(){
+		
+		String baseHome=System.getProperty("user.dir");
+		filePath=baseHome+File.separator+"conf"+File.separator+"config.properties";
+		if(log.isInfoEnabled()){
+			log.info("load config file: "+filePath);
+		}
+		
+		Properties props=new Properties();
+		try {
+			props.load(new FileInputStream(filePath));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+//		Properties props=System.getProperties();
+//		@SuppressWarnings("unchecked")
+//		Enumeration<String> names=(Enumeration<String>)props.propertyNames();
+//		while(names.hasMoreElements()){
+//			String name=names.nextElement();
+//			System.out.println(name+" = "+ System.getProperty(name));
+//		}
+		return props;
+	}
+	
+	public static String getParameter(String paramName){
+		return properties.getProperty(paramName);
+	}
+	
+	public static void main(String[] args) {
+//		properties=getProperties();
+		Enumeration<String> names=(Enumeration<String>)properties.propertyNames();
+		while(names.hasMoreElements()){
+			String name=names.nextElement();
+			System.out.println(name+" = "+ properties.getProperty(name));
+		}
+	}
+
+}
